1017ffac2947a0771d917379d5653fb15fe64082
Paul Hayes <paul@paul-hayes.net>
2025-03-29 15:56:52 -0700

    added factory for tickets

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
+use App\Models\User;
+// universal resource locator
+// tickets
+// users
+// contracts
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
